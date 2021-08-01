82988e3826d5fcf4718dbf73ec52ee6213aa3e2a
Stanislav <StanislavMelnikov48@gmail.com>
2021-08-01 14:05:30 +0300

    Add welcome_user func

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,1 +1,12 @@
-print("Welcome to the Brain Games!") 
+#!/usr/bin/env python
+
+def welcome_user():
+    import prompt
+    name = prompt.string('May i have your name?')
+
+def main():
+    print("Welcome to the Brain Games!") 
+    welcome_user()
+
+if __name__=='__main__':
+    main()
